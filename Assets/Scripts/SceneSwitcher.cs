6bcb49243dd83edd9bab1df497a2f07aaf431e39
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2019-11-15 12:41:09 -0800

    Link Test Now button to SmokeDetectorTestScene
    Connect the 2 scenes with a button.
    
    Co-Authored-By: Daniel Roy Greenfeld <pydanny@gmail.com>

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public void GoToSmokeDetectorTestScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorTestScene");
+    }
+
+}
