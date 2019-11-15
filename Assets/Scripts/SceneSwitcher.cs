3c82bd8802cb804c50ebb5b3c0bbb83db5352706
Daniel Roy Greenfeld <pydanny@gmail.com>
2019-11-15 14:21:59 -0800

    Added Smoke Detector to Quests
    - Reordered scripts to be alphabetical
    - Added `GoToSmokeDetectorPlacement` function
    - Added Smoke Detector quests section

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class SceneSwitcher : MonoBehaviour
 {
 
-    public void GoToSmokeDetectorTestScene()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorTestScene");
-    }
-
     public void GoTo3dScanHome()
     {
         Debug.Log("Going to 3d scan of home");
@@ -19,5 +14,19 @@ public class SceneSwitcher : MonoBehaviour
     {
         Debug.Log("Going to review of plan");
     }
+
+    public void GoToSmokeDetectorPlacement()
+    {
+        Debug.Log("Going to smoke detector placement");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorPlacement");
+    }
+
+
+    public void GoToSmokeDetectorTestScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorTestScene");
+    }
+
+    
 
 }
